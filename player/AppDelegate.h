2d9ef687de407bd3eaba97dac0f905a02e787f18
zloveqiang <zhangjianat@163.com>
2017-05-31 17:27:23 +0800

    Initial Commit

---
+++ player/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  player
+//
+//  Created by Mr.Zhang on 2017/5/31.
+//  Copyright © 2017年 Mr.Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ player/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  player
+//
+//  Created by Mr.Zhang on 2017/5/31.
+//  Copyright © 2017年 Mr.Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
